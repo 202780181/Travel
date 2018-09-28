5e0077678e715e3d90e43aa540491925fbfa9e8a
202780181@qq.com <202780181@qq.com>
2018-09-28 21:32:06 +0800

    'index-recomment'

---
+++ src/pages/Home/components/Recommend.vue
@@ -0,0 +1,141 @@
+<template>
+   <div>
+     <div class="recommend-title">
+       <img class="mp-title-img" src="http://img1.qunarzz.com/piao/fusion/1711/89/ebc329f16c55bb02.png" alt="猜你喜欢">
+       热销推荐</div>
+     <ul>
+       <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+         <img class="item-img" :src="item.imgUrl">
+         <div class="item-info">
+           <p class="item-title">{{item.title}}</p>
+           <p class="item-desc">
+             <span class="iconfont-t">
+               <i class="iconfont icon-xingxing"></i>
+               <i class="iconfont icon-xingxing"></i>
+               <i class="iconfont icon-xingxing"></i>
+               <i class="iconfont icon-xingxing"></i>
+               <i class="iconfont icon-xingxing"></i>
+              </span>
+             {{item.review}}条评论</p>
+           <div class="mp-like-price">
+             <span class="mpg-price">¥<em class="mpg-price-num">{{item.money}}</em></span>起
+             <span class="mp-like-address">{{item.area}}</span>
+           </div>
+         </div>
+       </li>
+     </ul>
+   </div>
+</template>
+
+<script>
+    export default {
+        name: "Recommend",
+      data (){
+         return {
+           recommendList:[{
+             id: '0001',
+             imgUrl: 'http://img1.qunarzz.com/sight/p0/1507/36/ce3d2d6c9ab44d67ae68d940b8781829.water.jpg_200x200_0938a8f2.jpg',
+             title: '北京野生动物园',
+             money: '69.8',
+             area: '大兴区',
+             review: '5444',
+             start:'5'
+           },{
+             id: '0002',
+             imgUrl:'http://img1.qunarzz.com/sight/p0/1508/89/895a1b7add84f23faca053ce9e3153db.water.jpg_200x200_99ae30ee.jpg',
+             title: '北京欢乐谷',
+             money: '49.9',
+             area: '朝阳区',
+             review: '56546',
+             start: '5'
+           },{
+             id: '0003',
+             imgUrl: 'http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_200x200_1bc99086.jpg',
+             title: '故宫',
+             money: '20',
+             area: '东城区',
+             review: '36354',
+             start: '5'
+
+           },{
+             id: '0004',
+             imgUrl: 'http://img1.qunarzz.com/sight/p0/1804/c8/c88bb5e69c9a9c5ea3.img.jpg_200x200_740720ff.jpg',
+             title: '古北水镇',
+             money: '150',
+             area: '密云区',
+             review: '359656',
+             start: '5'
+           },{
+             id: '0005',
+             imgUrl: 'http://img1.qunarzz.com/sight/p0/201308/08/41150dacc0e3a0c8c8d65eac.jpg_200x200_c62292e9.jpg',
+             title: '北京动物园',
+             money: '163',
+             area: '西城区',
+             review: '71156',
+             start: '5'
+           }]
+         }
+      }
+    }
+</script>
+
+<style lang="stylus" scoped>
+  .recommend-title
+    font-size: .44rem;
+    margin-top .3rem
+    line-height .8rem
+    background #eee
+    .mp-title-img
+      display: inline-block;
+      margin-top: 0.18rem;
+      overflow: hidden;
+      width: 0.5rem;
+      height: 0.5rem;
+      margin-left: 0.2rem;
+      vertical-align: top;
+  .item
+    overflow hidden
+    display flex
+    height 2.667rem
+    padding .2rem .1rem;
+    .item-img
+      height: 2.467rem
+      width 2.467rem
+      padding .1rem
+    .item-info
+      flex 1
+      padding .1rem
+      .item-title
+        margin-top .26rem
+        line-height .54rem
+        font-size .42rem
+      .item-desc
+        display flex
+        margin-top .2rem
+        font-size .24rem
+        line-height .4rem
+        color #616161
+        .iconfont-t
+          margin-right .3rem
+          display flex
+          .iconfont{
+            width .4rem
+            display block
+            color #ff8300
+          }
+
+      .mp-like-price
+        position relative
+        margin-top 0.27rem
+        color: #616161
+        font-size: .24rem
+        line-height: .4rem
+        .mpg-price
+          color: #ff8300
+          .mpg-price-num
+            font-size: .5rem
+        .mp-like-address
+          position: absolute
+          right: .24rem
+          bottom: 0
+</style>
---
+++ src/pages/Home/components/Weekend.vue
@@ -0,0 +1,100 @@
+<template>
+    <div class="lazy-load" id="weekend-trip">
+      <h2 class="mp-modtitle">周末去哪儿</h2>
+      <div>
+        <div class="mp-product-item" v-for="item of weekList" :key="item.id">
+          <a href="" :title="item.titleg" class="mp-fulllink">
+            <div class="product-imgcontainer image-ready">
+              <img :src="item.imgUrl">
+            </div>
+            <div class="mp-product-info">
+              <p class="product-name">{{item.Tname}}</p>
+              <p class="product-desc">{{item.Tdesc}}</p>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "Weekend",
+    data (){
+      return {
+        weekList: [{
+          id:'001',
+          title:'漂流勇进，烧烤露营',
+          href:'http://piao.qunar.com/touch/toSku.htm?skuId=367&skuName=漂流勇进，烧烤露营&cat=from_area%3Dts_sku%26from_index%3D0%26from_value%3D%25E6%25BC%2582%25E6%25B5%2581%25E5%258B%2587%25E8%25BF%259B%25EF%25BC%258C%25E7%2583%25A7%25E7%2583%25A4%25E9%259C%25B2%25E8%2590%25A5%26dist_city%3D%25E5%258C%2597%25E4%25BA%25AC',
+          imgUrl:'http://img1.qunarzz.com/sight/source/1606/85/c6b6bad1b977e.jpg_r_640x214_5c638941.jpg',
+          Tname:"漂流勇进，烧烤露营",
+          Tdesc:'轻松水上漂，任我乐逍遥'
+        },{
+          id:'002',
+          title:'北京清凉避暑胜地',
+          href:'http://piao.qunar.com/touch/toSku.htm?skuId=366&skuName=北京清凉避暑胜地&cat=from_area%3Dts_sku%26from_index%3D1%26from_value%3D%25E5%258C%2597%25E4%25BA%25AC%25E6%25B8%2585%25E5%2587%2589%25E9%2581%25BF%25E6%259A%2591%25E8%2583%259C%25E5%259C%25B0%26dist_city%3D%25E5%258C%2597%25E4%25BA%25AC',
+          imgUrl:'http://img1.qunarzz.com/sight/source/1505/53/02091f1ee43d0a.jpg_r_640x214_2e3256b1.jpg',
+          Tname:"北京清凉避暑胜地",
+          Tdesc:'到大自然中享受“天然空调”的清凉'
+        },{
+          id:'003',
+          title:'北京必游TOP10',
+          href:'http://piao.qunar.com/touch/toSku.htm?skuId=256&skuName=北京必游TOP10&cat=from_area%3Dts_sku%26from_index%3D2%26from_value%3D%25E5%258C%2597%25E4%25BA%25AC%25E5%25BF%2585%25E6%25B8%25B8TOP10%26dist_city%3D%25E5%258C%2597%25E4%25BA%25AC',
+          imgUrl:'http://img1.qunarzz.com/sight/source/1505/aa/7baaf8a851d221.jpg_r_640x214_1431200f.jpg',
+          Tname:"北京必游TOP10",
+          Tdesc:'来北京必去的景点非这些地方莫属'
+        }]
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+    #weekend-trip
+      .mp-modtitle
+        height .8rem
+        padding-left .26rem
+        line-height .8rem
+        color #212121
+        font-size .35rem
+      .mp-product-item
+          position relative
+          margin-bottom .1rem
+          background #fff
+          .mp-fulllink
+            display block
+            width 100%
+            height 100%
+            .image-ready
+              overflow: hidden;
+              height: 0;
+              padding-bottom: 37.4375%;
+              background: url(//s.qunarzz.com/piao_topic/image/common/default/640x266.jpg) no-repeat center #dcdcdc;
+              background-size: 100% 100%;
+          .image-ready
+            img
+              opacity 1
+              transition opacity 400ms
+              width: 100%
+
+          .mp-product-info
+            position relative
+            padding .14rem .2rem .2rem .2rem
+            .product-name
+              overflow hidden
+              padding-right 1.4rem
+              color: #212121
+              font-size .38rem
+              line-height .68rem
+              white-space nowrap
+              text-overflow ellipsis
+            .product-desc
+              overflow hidden
+              padding-right 1.4rem
+              color #616161
+              font-size .34rem
+              line-height .62rem
+              white-space nowrap
+              text-overflow ellipsis
+
+</style>
