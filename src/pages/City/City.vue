b9ca6e3f848350df0cff446a766b62dd3c61e78b
202780181@qq.com <202780181@qq.com>
2018-10-01 21:27:18 +0800

    city list

--- src/pages/City/City.vue
+++ src/pages/City/City.vue
@@ -2,17 +2,23 @@
     <div>
       <city-header></city-header>
       <search></search>
+      <city-list></city-list>
+      <Alphabet></Alphabet>
     </div>
 </template>
 
 <script>
   import cityHeader from './components/Header'
   import Search from './components/search'
+  import CityList from './components/list'
+  import Alphabet from './components/Alphabet'
     export default {
         name: "City",
       components:{
         cityHeader,
-        Search
+        Search,
+        CityList,
+        Alphabet
       }
     }
 </script>
